9b30b6e8de0e4e3720c6c9c37789bb1358d1f60b
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-21 00:50:33 +0530

    Added :Add the element in linkedlist

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -3,4 +3,26 @@ package com.bridgelabz;
 public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
+
+    public void add(T data) {
+        Node<T> newNode=new Node<>(data);
+        if (newNode==null){
+            head=newNode;
+            tail=newNode;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+
+    public void display() {
+       Node<T> currentNode=head;
+       if (currentNode==null){
+           System.out.println("Linked list is empty");
+       }
+       while (currentNode !=null){
+           System.out.print(currentNode.data+" -> ");
+           currentNode=currentNode.next;
+       }
+
+    }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,6 +4,10 @@ public class LinkedListMain<T> {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Program");
         LinkedList<Integer> linkedList=new LinkedList<>();
+        linkedList.add(70);
+        linkedList.add(30);
+        linkedList.add(56);
+        linkedList.display();
 
 
 
