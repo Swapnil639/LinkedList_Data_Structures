eaf3071519704f63e5fd64dc13a379e093f3af79
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-20 18:36:20 +0530

    Added :Welcome Message

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LinkedList<E> {
+    Node<E> head;
+    Node<E> tail;
+
+
+}
---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LinkedListMain<T> {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Program");
+
+    }
+}
